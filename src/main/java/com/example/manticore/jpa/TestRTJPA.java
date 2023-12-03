aa1792771349602c37d3e75bf49c3fd5be27e0e4
guofengzh <guofengzh@gmail.com>
2023-12-03 17:52:23 +0800

    Initial commit

--- src/main/java/com/example/manticore/jpa/TestRTJPA.java
+++ src/main/java/com/example/manticore/jpa/TestRTJPA.java
@@ -1,7 +1,11 @@
 package com.example.manticore.jpa;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "testrt")
 public class TestRTJPA {
@@ -14,36 +18,4 @@ public class TestRTJPA {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Integer getGid() {
-        return gid;
-    }
-
-    public void setGid(Integer gid) {
-        this.gid = gid;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
